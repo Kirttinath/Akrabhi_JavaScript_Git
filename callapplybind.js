1557d46d4e9066de481ea0d821fe307de8cf4687
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 01:15:48 +0530

    this refers to an object instead of window

--- callapplybind.js
+++ callapplybind.js
@@ -1,2 +1,11 @@
 // call : 
 //If we have a function and object and  you have to run function on that object or we put this keyword which reference to the window to change that reference to another object
+
+function newf()
+{
+    console.log(this);
+}
+ var obj = {
+    age: 24
+ }
+ newf.call(obj);
