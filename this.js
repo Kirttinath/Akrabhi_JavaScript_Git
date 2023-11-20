bb5fa8d7aaeb8dacf73aa87a18928d47f54d08b9
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 21:03:32 +0530

    This  keyword uses in many type scopes

--- this.js
+++ this.js
@@ -3,3 +3,15 @@
 //In Global Scope :
 
 console.log(this);// Refers to window
+
+// In function scope
+console.log(this);// Refers to window
+
+// In method scope 
+ var obj = {
+    name: "Kirtti";
+    somemethd: function()
+    {
+    console.log(this); // Refers to  Object obj
+ }
+}
