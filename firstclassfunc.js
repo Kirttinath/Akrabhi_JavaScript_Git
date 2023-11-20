62b6419ce6caf37fae677bc5bdda815be08bd060
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 20:19:15 +0530

    explaination like setTimeout is a firstcclass function

--- firstclassfunc.js
+++ firstclassfunc.js
@@ -1,3 +1,17 @@
 // First Class Function : 
 // A function is a value that can be assigned to a variable and passed as an argument to another function.
 // It's also a type of object in JavaScript, with properties and methods just like any other object.
+ 
+function firstcls()
+{
+
+}
+firstcls(function ()
+{
+
+});
+ // or 
+ setTimeout(function()
+ {
+
+ }, 3000);
