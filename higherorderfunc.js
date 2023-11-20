b756b4d340abf808d595fc13b19e9764ad820fcd
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 19:31:00 +0530

    forEach function is a higher order function

--- higherorderfunc.js
+++ higherorderfunc.js
@@ -1,7 +1,15 @@
 //Higher Order Function :
 //A function  which accept a function as a parameter
 //or Return a function or Both
-function f()
+//for ex  : ForEach Method alwaays takes another function inside it
+//So forEach is a higher order function
+function f(val)
 {
      
 }
+f( function fx(){
+
+})
+//or ex:
+var arr = [1,2,3,4,5];
+arr.forEach(function(){})
