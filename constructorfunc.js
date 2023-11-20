df63d4bc10237a67f1dcbd39114c79984980e51b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 19:54:06 +0530

    constructor function defination

---
+++ constructorfunc.js
@@ -0,0 +1,7 @@
+//Consttructor Function :
+//A constructor function is a special type of JavaScript function that is used to create and initialize an object created with the new keyword. Constructor functions have
+
+function constr()
+{
+   
+}
