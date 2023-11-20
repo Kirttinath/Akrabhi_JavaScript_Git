cc6da532ad925bbac6db29038915de04de341b63
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 20:31:35 +0530

    new keyword function derivation

--- new.js
+++ new.js
@@ -1,2 +1,11 @@
 // New Keyword : 
 // This keyword is used to create a new object. It can be used with any class or struct type, and it creates an instance of that class
+
+
+function newf()
+{
+    this.age = 18;
+
+}
+new newf();
+console.log(this);
