57f5f0415039cbffed9fe8a51c61dd2686d467b4
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 17:55:44 +0530

    Global Execution Context explained

---
+++ execontext.js
@@ -0,0 +1,10 @@
+var x=2;
+function square(num)
+{
+    var res =  num*num;
+    return res;
+}
+var sq1 = square(x);
+var sq2 = square(4);
+console.log("Square of 2 = "+sq1);
+console.log("Square of 4 = "+sq2);
