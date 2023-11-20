95ebab63aa572f3ea6654f0de67aee9a4f6ddf27
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 18:00:02 +0530

    Hoisting of let const and var in javascript

---
+++ letconsthoist.js
@@ -0,0 +1,22 @@
+//let a = 100;
+{
+    let a = 10;
+    //let a=190;
+    const b = 9;
+    //const b=27;
+    var n = 19;
+    console.log(a);
+    console.log(b);
+    console.log(n);
+
+}
+//let a=16;  // syntax error ; a already declared
+//const a=14;  // syntax error ; a already declared
+//const b = 1005;
+//var n = 12;
+console.log(n);
+
+console.log(a);
+console.log(b);
+console.log(n);
+
