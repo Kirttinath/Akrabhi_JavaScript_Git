e0b760c12cf33246460cd393f86bd60a439ca18b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 02:07:21 +0530

    Value vs reference variable in JS

---
+++ valuevsref.js
@@ -0,0 +1,3 @@
+// Value VS Reference Variables 
+// In Javascript always assigns variables by value. But this part is very important: when the assigned value is one of JavaScript’s five primitive type (i.e., Boolean, null, undefined, String, and Number) the actual value is assigned. However, when the assigned value is an Array, Function, or Object a reference to the object in memory is assigned
+
