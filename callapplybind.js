33ef36357f23b003183472577c768e237aaf2e13
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 01:13:52 +0530

    call apply bind

---
+++ callapplybind.js
@@ -0,0 +1,2 @@
+// call : 
+//If we have a function and object and  you have to run function on that object or we put this keyword which reference to the window to change that reference to another object
