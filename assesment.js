548fdf0eeac511f0f74f4e2d2761a0af89f09bde
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 17:54:06 +0530

    assesment for javascript

---
+++ assesment.js
@@ -0,0 +1,51 @@
+//promise function
+
+  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+  // The producing code (this may take some time)
+  
+    if (x == 0) {
+      myResolve("OK");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then(
+    function(value) {console.log(value);},
+    function(error) {console.log(error);}
+  );
+
+  //asynchronous 
+  async function myFunction() {
+    return "Hello";
+  }
+  myFunction().then(
+    function(value) {console.log(value);},
+    function(error) {console.log(error);}
+  );
+  
+
+  //await
+  async function myDisplay() {
+    let myPromise = new Promise(function(resolve, reject) {
+      resolve("Kirtti");
+    });
+    console.log( await myPromise);
+  }
+  
+  myDisplay();
+
+  //
+  function myFirst() {
+    console.log("Kirttinath");
+  }
+  
+  function mySecond() {
+    console.log("Goodbye");
+  }
+  mySecond();//this will execute first
+  myFirst();//second this is execute
+  
