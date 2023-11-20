b298bf24a4134dee4bcd82f392a10e0e4978e2c5
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 01:22:53 +0530

    access this value of object by call with passing arguments of object and function parameters

--- callapplybind.js
+++ callapplybind.js
@@ -1,11 +1,11 @@
 // call : 
 //If we have a function and object and  you have to run function on that object or we put this keyword which reference to the window to change that reference to another object
 
-function newf()
+function newf(val1,val2, val3)
 {
-    console.log(this.age);
+    console.log(this.age,val1,val2,val3);
 }
  var obj = {
     age: 24
  }
- newf.call(obj);
+ newf.call(obj,1,2,3);
