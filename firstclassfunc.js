bec66c4eefad1e3b64ad9476949dbe4cb06605f9
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 20:14:13 +0530

    First class function defination

---
+++ firstclassfunc.js
@@ -0,0 +1,3 @@
+// First Class Function : 
+// A function is a value that can be assigned to a variable and passed as an argument to another function.
+// It's also a type of object in JavaScript, with properties and methods just like any other object.
