2c6463dc5142326ad91b4658d8eefa8b6745441a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 20:48:21 +0530

    derivation for prototypes

--- prototype.js
+++ prototype.js
@@ -1,3 +1,6 @@
 // Prototype  in JS : 
 //[[prototype]] contains many helper properties and methods which we can use to complete our task, let's say we create an array and we want to know length of it, what do we do, we use length property on array, did we created length on that array, no! but it still contains .length
 //many properties and methods are already available to use built by javascript creators inside prototype of every object
+
+//in browser console create 
+//var obj = { name: "Kirtti"} now call obj.
