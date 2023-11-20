fc21f4829359be7ca22b735af595832474b949cd
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 20:38:20 +0530

    derivation of life by getter and setter in JS

--- life.js
+++ life.js
@@ -1,3 +1,19 @@
 //Life in Javascript : 
 // Immidiately invoked function expression
 
+
+var ans = (function()
+{
+    var prvtval =12;
+    return {
+        getter: function()
+        {
+            console.log(prvtval);
+        },
+        setter: function(value)
+        {
+            prvtval = val;
+        }
+    }
+
+})()
