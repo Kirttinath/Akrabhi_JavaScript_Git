5f9b93cc6135d1ae368071a9ab603925c403cd4f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-20 17:58:56 +0530

    Let Const Variable explained in Javascript

---
+++ letconst.js
@@ -0,0 +1,9 @@
+{
+    let a = 10;
+    const b = 100;
+    var c = 1000;
+
+}
+//console.log(a); //ReferenceError: a is not defined
+//console.log(b); // ReferenceError: b is not defined 
+console.log(c);
