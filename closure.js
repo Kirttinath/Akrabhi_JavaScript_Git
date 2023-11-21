92dd3ef2d5acce09ae6c0a947138d739b806ab23
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 11:37:02 +0530

    envision an initial function apiConnect(apiKey) that returns some methods that would use the API key

--- closure.js
+++ closure.js
@@ -7,7 +7,7 @@ function f1() {
     function f2() {
         console.log(a);
     }
-   
+
     return f2;
 
 }
@@ -17,14 +17,28 @@ c();
 
 //Closures :
 //Closure is an important JavaScript pattern to give private access to a variable
-function closures(val)
-{
-   return function (name)
-   {
-     console.log(val+" "+name);
-   }
+function closures(val) {
+    return function (name) {
+        console.log(val + " " + name);
+    }
 }
 const greet = closures("Hello");
 greet("Kirtti");
 
+//In a more real-world scenario, you could envision an initial function apiConnect(apiKey) that returns some methods that would use the API key. In this case, the apiKey would just need to be provided once and never again.
 
+function apiConnect(apiKey) {
+    function get(route) {
+        return fetch(`${route}?key=${apiKey}`);
+    }
+
+
+
+    function post(route, params) { 
+        return fetch(route, { method: 'POST', body: JSON.stringify(params), headers: { 'Authorization': `Bearer ${apiKey}` } }) }
+
+
+    return { get, post }
+}
+
+const api = apiConnect('my-secret-key');
