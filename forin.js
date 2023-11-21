121d3d7bad885cf7d4dd451690a30ced833a8750
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 18:16:42 +0530

    for in loop derivation with example

--- forin.js
+++ forin.js
@@ -1,3 +1,13 @@
 //For in  Loop :
 //for in loop is used for access all the properties of an object
 
+var obj = {
+    name: "Kirtti",
+    age:  24,
+    city: "BBSR"
+}
+for (var key in obj)
+{
+    console.log(key+" : "+obj[key]);
+
+}
