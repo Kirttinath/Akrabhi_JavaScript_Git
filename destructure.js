072351679776a7fd7b4c381121397f10ab5a1b89
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 11:48:03 +0530

    extract properties under a different name, you can specify them using destructuring

--- destructure.js
+++ destructure.js
@@ -1,1 +1,13 @@
 //Destructure : 
+// It is a javascript expression that makes it possible to unpack values from arrays, or properties from objects , into distinct variables
+const obj = {name: 'Kirtti',food: 'veg'}
+const { name, food } = obj;
+console.log(name,food);
+
+//If you want to extract properties under a different name, you can specify them using the following format.
+const obj1 = {name: 'Kirtti',food: 'veg'}
+
+const { name: myName, food: myFood } = obj;
+
+
+console.log(myName, myFood);
