c0a3e48d955afafa7cfb6fe6610dfed1151ce48d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 12:15:24 +0530

    Definition of map filter and reduce methods

---
+++ arraymethods.js
@@ -0,0 +1,4 @@
+//Array Methods <map, filter, reduce>
+//Map: Create a new array with the results of calling a provided function on every element in this array.
+//filter: return array of elements where the function returns true
+//reduce: accumulate values as specified in function
