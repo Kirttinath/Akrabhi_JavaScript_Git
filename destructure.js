833344beda5bf8ce2228df81f16a36a4aa0ba8dd
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 11:51:12 +0530

    destrucuring used directly for extracting parameters passed to a function

--- destructure.js
+++ destructure.js
@@ -1,13 +1,20 @@
 //Destructure : 
 // It is a javascript expression that makes it possible to unpack values from arrays, or properties from objects , into distinct variables
-const obj = {name: 'Kirtti',food: 'veg'}
+const obj = { name: 'Kirtti', food: 'veg' };
 const { name, food } = obj;
-console.log(name,food);
+console.log(name, food);
 
 //If you want to extract properties under a different name, you can specify them using the following format.
-const obj1 = {name: 'Kirtti',food: 'veg'}
+const obj1 = { name: 'Kirtti', food: 'veg' };
 
 const { name: myName, food: myFood } = obj;
 
 
 console.log(myName, myFood);
+
+// destructuring can be (and often is) used directly for extracting parameters passed to a function.
+const person = { name: 'Kirtti', age: 24 };
+function introduce({ name, age }) {
+    console.log(`I'm ${name} and I'm ${age} years old!`);
+}
+introduce(person);
