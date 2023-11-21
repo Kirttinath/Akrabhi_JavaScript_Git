93497df37d8b6e959c7f473b1ffc2b499181b29f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 20:43:17 +0530

    another callback  definition with example  and derivation

--- callbackfunc.js
+++ callbackfunc.js
@@ -12,3 +12,15 @@ setTimeout(function(){
     console.log("Hii Kirtti");
 },2000);
 
+
+//A callback is a function that is passed as an argument to another function, and is called after the main function has finished its execution. The main function is called with a callback function as its argument, and when the main function is finished, it calls the callback function to provide a result. Callbacks allow you to handle the results of an asynchronous operation in a non-blocking manner, which means that the program can continue to run while the operation is being executed.
+
+const greeting = (name) => {
+	console.log('Hello' + name);
+}
+
+const processUserName = (callback) => {
+	name = 'Kirtti';
+	callback(name);
+}
+processUserName(greeting);
