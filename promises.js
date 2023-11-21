6fedee1e8f2bba46fae944faee2209e9537a5eaf
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 20:56:47 +0530

    promises is a solution for callback hell situation

--- promises.js
+++ promises.js
@@ -14,3 +14,19 @@ myPromise.then(function(data) {
     console.log('Success: ' + data);}).catch(function(err) {console.log('Error: ' + err);});
 
 // If Math.random() returns less than 0.9 the following is logged:// "Success: Hooray!"// If Math.random() returns 0.9 or greater the following is logged:// "Error: On no!"
+
+//A Promise is an object representing the eventual completion or failure of an asynchronous operation. Essentially, a promise is a returned object to which you attach callbacks, instead of passing callbacks into a function
+
+//Promises resolve the issue of ‘callback hell’ which is nothing but a recursive structure of callbacks (callbacks within callbacks within callbacks and so forth). 
+
+const promise = new Promise((resolve, reject) => {
+	isNameExist = true;
+	if (isNameExist) {
+		resolve("User name exist")
+	} else {
+		reject("error")
+	}
+})
+promise.then(result => console.log(result)).catch(() => {
+			console.log('error!');
+			})
