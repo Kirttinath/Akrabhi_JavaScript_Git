97ef3264e42d2aba0bc1e045eb9162b698827c30
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 11:56:51 +0530

    spraedsyntax

---
+++ spreadsyntax.js
@@ -0,0 +1,3 @@
+//Spread Syntax : 
+//The spread syntax allows an iterable (like arrays and strings) to be expanded in places where zero or more arguments are expected.
+// Math.max can't be applied to the arr array because it doesn't take an array as an argument, it takes the individual elements as arguments. The spread operator ... is used to pull the individual elements out the array.
