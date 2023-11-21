74ce228dffafd9e6dc7552d9b84b06f9f9724aa6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 17:24:44 +0530

    forEach derivation and explaination with example

--- foreach.js
+++ foreach.js
@@ -1,3 +1,9 @@
 //For Each loop :
 //This is a simple for each loop that iterates over an array of numbers. It prints out the index and value of each element in the array
-//
+
+const a = [1,2,3,4,5];
+a.forEach(function(val)
+{
+console.log(val*2);
+});
+// forEach shouldn't change the default array
