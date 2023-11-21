84d661a81a506da8afb235b53a8ce26812fccf3b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 14:22:28 +0530

    Polyfills derivation and explanation with example

--- transpilers.js
+++ transpilers.js
@@ -6,3 +6,16 @@
 height ?? 100;// in Morden code  nullish coalescing operator
 // After use transpilers 
 (height !== undefined && height !==null) ? height : 100;
+
+
+// Polyfills
+// A script that updates/adds new function  is called Polyfills. It fills the gap and missing Implimentation
+
+if (!Math.trunc) { // if no such function
+    // implement it
+    Math.trunc = function(number) {
+      // Math.ceil and Math.floor exist even in ancient JavaScript engines
+      
+      return number < 0 ? Math.ceil(number) : Math.floor(number);
+    };
+  }
