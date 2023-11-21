2e4203445c6f49bdcef2de80a44ca0765b22643b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 13:43:25 +0530

    async and await derivation with example

--- asyncawait.js
+++ asyncawait.js
@@ -3,3 +3,11 @@
 // The async keyword before the function definition allows us to use await inside it, 
 // which makes JavaScript wait for an asynchronous operation to complete before continuing with the rest of the code.
 
+const greeter = new Promise((res, rej) => {
+    setTimeout(() => res('Hello world!'), 2000);
+});
+async function myFunc() {
+    const greeting = await greeter;
+    console.log(greeting);
+}
+myFunc();// 'Hello world!'
