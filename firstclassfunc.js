c55fba387c359af6aea2a092e7bac0608f59a260
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 19:19:00 +0530

    function is a value that can be assigned to a variable

--- firstclassfunc.js
+++ firstclassfunc.js
@@ -2,16 +2,11 @@
 // A function is a value that can be assigned to a variable and passed as an argument to another function.
 // It's also a type of object in JavaScript, with properties and methods just like any other object.
  
-function firstcls()
-{
+var a = function firstcls(){};
 
-}
-firstcls(function ()
-{
 
-});
+firstcls(function (){});
  // or 
- setTimeout(function()
- {
 
- }, 3000);
+ 
+ setTimeout(function(){}, 3000);
