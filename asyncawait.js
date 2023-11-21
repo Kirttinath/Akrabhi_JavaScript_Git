3c29fe63473e35f4b6f82660ebb64f6f3f286171
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 13:40:19 +0530

    Async and await function definition

---
+++ asyncawait.js
@@ -0,0 +1,5 @@
+//Async and Await :
+// Async function is a special type of function that can be used to create asynchronous operations.
+// The async keyword before the function definition allows us to use await inside it, 
+// which makes JavaScript wait for an asynchronous operation to complete before continuing with the rest of the code.
+
