36992d79df78b6bf9c4f173290441c3b3ad8a11f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 13:32:41 +0530

    promises defination and their states

---
+++ promises.js
@@ -0,0 +1,4 @@
+//Promises :
+//A Promise is an object representing the eventual completion or failure of an asynchronous operation and its resulting value. Promises are a way to handle asynchronous
+//A promise is an object that may produce a single value sometime in the future.
+//It has three states: pending, fulfilled (with data), and rejected (with error).
