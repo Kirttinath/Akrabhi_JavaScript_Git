5e2ee4912a7da8adde919db3ead7ab0d8f6fec09
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 20:31:49 +0530

    simple closure property by arrow function

--- closures.js
+++ closures.js
@@ -1,3 +1,15 @@
 //closures :
 //Closure is a function object that has access to its own scope, the outer function’s variables. Three characteristics of closures are:
 
+const a=10;
+const f = () => 
+{
+  const a=23;
+  const f2 = () =>
+  {
+    console.log(a);//23
+  }
+  f2();
+}
+f();
+console.log(a);//10
