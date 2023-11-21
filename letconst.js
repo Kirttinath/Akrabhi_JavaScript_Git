0b2e6b84554fbc2b68c9772b71a075580c190b4d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 15:18:38 +0530

    difference of var and let by scoping

--- letconst.js
+++ letconst.js
@@ -7,3 +7,15 @@
 //console.log(a); //ReferenceError: a is not defined
 //console.log(b); // ReferenceError: b is not defined 
 console.log(c);
+
+// Difference Between var and let
+//1.  var is function scoped (ES5)
+//    let is braces scoped   (ES6)
+function diff()
+{
+    for(var i=1;i<=10;i++)
+    {
+        console.log(i);
+    }
+    console.log(i);
+}
