7e8abe6ee3be07407d4d25b511e673e8f915eaee
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 12:54:30 +0530

    how yield works in generator functions

--- generatorfunc.js
+++ generatorfunc.js
@@ -1,2 +1,19 @@
-//Generator function ;
+//Generator function '*';
 //The generator function specifies what value is yielded next time next() is called. Can either have a finite number of yields, after which next() returns an undefined value, or an infinite number of values using a loop.
+
+function* greeter() {
+    yield "Kirtti";
+    yield "How are you";
+    yield "fine?";
+}
+const greet = greeter();
+console.log(greet.next().value);// 'Kirtti'
+console.log(greet.next().value);// 'How are you?'
+console.log(greet.next().value);// 'fine'
+console.log(greet.next().value);// undefined
+
+function* idCreator() {let i = 0;while (true)yield i++;}
+const ids = idCreator();
+console.log(ids.next().value);// 0
+console.log(ids.next().value);// 1
+console.log(ids.next().value);// 2
