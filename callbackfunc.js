c5e7bd13dbb3f8308f57fabbb7a09dbf237116b6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 19:05:07 +0530

    another callback function definition with derivation of setTimeout call back function

--- callbackfunc.js
+++ callbackfunc.js
@@ -3,5 +3,12 @@
 function myFunc(text, callback) {
     setTimeout(function() {
         callback(text);
-    }, 3000);}
+    }, 1000);}
 myFunc('Hello Kirtti!', console.log);// 'Hello Kirtti!'
+
+//A callback function is a function that is passed as an argument to another function and is executed after the completion of a specific task. 
+//Callbacks are commonly used in asynchronous operations, such as handling events, making API requests, or reading files
+setTimeout(function(){                                                  
+    console.log("Hii Kirtti");
+},2000);
+
