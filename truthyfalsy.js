899ba1d30f516120858bd70f7f5df794736915d9
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 17:04:06 +0530

    truthy and falsy values description and derivation with example

--- truthyfalsy.js
+++ truthyfalsy.js
@@ -1,3 +1,10 @@
 //Falsy values : 0 , undefined , null , NaN , false , document.all
 //Truthy :  Except those values , all values are "Truthy"
 
+if(false)
+{
+    console.log("Hello");
+}
+else{
+    console.log("World");
+}
