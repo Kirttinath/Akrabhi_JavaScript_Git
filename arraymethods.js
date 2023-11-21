ecefbc3cb43ba82a28e700fc595111dbb6cb4167
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-21 12:18:24 +0530

    map derivation with example

--- arraymethods.js
+++ arraymethods.js
@@ -2,3 +2,8 @@
 //Map: Create a new array with the results of calling a provided function on every element in this array.
 //filter: return array of elements where the function returns true
 //reduce: accumulate values as specified in function
+
+// Map :
+const arr = [1, 2, 3, 4, 5];
+const newarr = arr.map(num => num * 5);
+console.log(newarr);
