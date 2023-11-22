343c66c10a61ea6c4d1ea0300110c31fc804e98d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-22 12:49:46 +0530

    hoisting is the difference between function statement and expression

--- function.js
+++ function.js
@@ -5,15 +5,19 @@
 
 //Function Statement :
 //A function is declared using the keyword "function" followed by its name and parameters enclosed within parentheses. The code to be executed by the function
+a();//Hello
+b();//error : b is not a function
 function a()
 {
     console.log("Hello");
 }
-a();//Hello
 
+//But the difference between statement and expression is hoisting
 //Function Expression :
 var b = function()
             {
                 console.log("World");
             }
-b();//world
+
+
+
