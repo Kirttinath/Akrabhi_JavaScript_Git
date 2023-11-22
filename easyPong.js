f5df1ffb997344db8694f117de3903aa9c2f1f0b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-22 19:59:06 +0530

    Draw the rectangle behind the 'Press any key to begin' text.

--- easyPong.js
+++ easyPong.js
@@ -39,3 +39,40 @@ var ball = {
         };
     }
 };
+
+var Game = {
+    initialize: function () {
+        this.canvas = document.querySelector('canvas');
+        this.context = this.canvas.getContext('2d');
+ 
+        this.canvas.width = 1400;
+        this.canvas.height = 1000;
+ 
+        this.canvas.style.width = (this.canvas.width / 2) + 'px';
+        this.canvas.style.height = (this.canvas.height / 2) + 'px';
+ 
+        this.player = Ai.new.call(this, 'left');
+        this.ai = Ai.new.call(this, 'right');
+        this.ball = Ball.new.call(this);
+ 
+        this.ai.speed = 5;
+        this.running = this.over = false;
+        this.turn = this.ai;
+        this.timer = this.round = 0;
+        this.color = '#8c52ff';
+ 
+        Pong.menu();
+        Pong.listen();
+    },
+    endGameMenu: function (text) {
+        // Change the canvas font size and color
+        Pong.context.font = '45px Courier New';
+        Pong.context.fillStyle = this.color;
+ 
+        // Draw the rectangle behind the 'Press any key to begin' text.
+        Pong.context.fillRect(
+            Pong.canvas.width / 2 - 350,
+            Pong.canvas.height / 2 - 48,
+            700,
+            100
+        );
