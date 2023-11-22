67a779d5b37410c66c06945435d72caffaa1f692
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-22 20:00:13 +0530

    Draw the 'press any key to begin' text

--- easyPong.js
+++ easyPong.js
@@ -76,3 +76,43 @@ var Game = {
             700,
             100
         );
+        / Change the canvas color;
+        Pong.context.fillStyle = '#ffffff';
+ 
+        // Draw the end game menu text ('Game Over' and 'Winner')
+        Pong.context.fillText(text,
+            Pong.canvas.width / 2,
+            Pong.canvas.height / 2 + 15
+        );
+ 
+        setTimeout(function () {
+            Pong = Object.assign({}, Game);
+            Pong.initialize();
+        }, 3000);
+    },
+ 
+    menu: function () {
+        // Draw all the Pong objects in their current state
+        Pong.draw();
+ 
+        // Change the canvas font size and color
+        this.context.font = '50px Courier New';
+        this.context.fillStyle = this.color;
+ 
+        // Draw the rectangle behind the 'Press any key to begin' text.
+        this.context.fillRect(
+            this.canvas.width / 2 - 350,
+            this.canvas.height / 2 - 48,
+            700,
+            100
+        );
+ 
+        // Change the canvas color;
+        this.context.fillStyle = '#ffffff';
+ 
+        // Draw the 'press any key to begin' text
+        this.context.fillText('Press any key to begin',
+            this.canvas.width / 2,
+            this.canvas.height / 2 + 15
+        );
+    },
