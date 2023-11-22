3c626762a0a6d437e69c89774d1b9b3069bef82f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-22 11:35:36 +0530

    we can't access the "a" in global which placed inside a function

--- datahiding.js
+++ datahiding.js
@@ -1,3 +1,11 @@
 //Data Hiding / Encapsulation by closure 
 //Encapsulating the data and providing access to it through methods.
 //Example :
+
+function dataout() {
+    var a = 100;
+    function datahide() {
+        a++;
+    }
+}
+console.log(a);
