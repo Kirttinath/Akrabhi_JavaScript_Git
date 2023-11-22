8ca67d48d67a1bb1621b212e1bbc457433b082d7
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-22 19:29:13 +0530

    create ball object for ping that

--- easyPong.js
+++ easyPong.js
@@ -10,3 +10,18 @@ var DIRECTION = {
 var rounds = [5,5,3,3,2];
 var colors = ['#1abc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6'];
 
+//The ball object (code that bounces back and forth)
+var ball = {
+    new: function(incrementedSpeed) {
+        return {
+            width: 18,
+            height: 18,
+            x: (this.canvas.width / 2)-9,
+            y: (this.canvas.height / 2)-9,
+            moveX: DIRECTION.IDLE,
+            moveY: DIRECTION.IDLE,
+            speed: incrementedSpeed || 7
+        };
+    }
+};
+
