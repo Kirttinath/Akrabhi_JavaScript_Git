ffecd16909afe56696a68b0238c33de2974a8b53
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-23 20:58:03 +0530

    definition of try and catch with example

---
+++ errorhandling.js
@@ -0,0 +1,11 @@
+//The try and catch  has two main blocks: try, and then catch
+//try and catch only works for runtime errors
+
+try {
+   // .;
+} catch (err) {
+    console.log("The engine can't understand this code, it's invalid");
+    //It won’t work if the code is syntactically wrong
+}
+
+
