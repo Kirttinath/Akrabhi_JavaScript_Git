439e2fd18bff911646a4a122547b3210529e9495
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-23 17:03:07 +0530

    start and stop button operation

--- projectasync1.js
+++ projectasync1.js
@@ -1,22 +1,43 @@
 
-const changeobj = function()
-{
-    document.querySelector("#head1").innerHTML = "Async Javascript";
-}
+// const changeobj = function()
+// {
+//     document.querySelector("#head1").innerHTML = "Async Javascript";
+// }
 
-const timer = function(){
+// const timer = function(){
+//     console.log("Kirtti");
+// };
+// const stopobj = setTimeout(changeobj, 2000);
+// document.querySelector("#stop").addEventListener("click", function()
+// {
+//     clearTimeout(stopobj);
+//     console.log("Stopped");
+// })
+
+// document.querySelector("#resume").addEventListener("click", function(){
+//     setTimeout(changeobj,2000);
+//     console.log("Resumed");
+// })
+
+//setInterval : Means at a same  interval time work this
+//ex:
+let startstop;
+
+const starting  = function(){
     console.log("Kirtti");
 };
-const stopobj = setTimeout(changeobj, 2000);
-document.querySelector("#stop").addEventListener("click", function()
-{
-    clearTimeout(stopobj);
+
+
+document.querySelector("#start")
+.addEventListener("click", function(){
+    
+     startstop = setInterval(starting,1000);
+     console.log("Started");
+})
+
+ 
+
+document.querySelector("#stop").addEventListener("click", function(){
+    clearInterval(startstop);
     console.log("Stopped");
 })
-
-document.querySelector("#resume").addEventListener("click", function(){
-    setTimeout(changeobj,2000);
-    console.log("Resumed");
-})
-
-
