49888de27a06dd3a66ae435bde4644b2612c83c6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-23 11:57:10 +0530

    button clicked - on function executed

--- callBackFunction.js
+++ callBackFunction.js
@@ -3,7 +3,21 @@
 //And this called callback function means that when we required that function we can callback it to execute
 //ex 
 
+setTimeout(function()
+{
+   console.log("after 5 sec print it");
+},5000);
 
+function x(z)
+{
+   console.log("Hello");
+   z();
+
+}
+
+x(function y(){
+    console.log("world");
+});
 
 document.getElementById("btn").addEventListener("click", function xyz(){
     console.log("Kirtti");
