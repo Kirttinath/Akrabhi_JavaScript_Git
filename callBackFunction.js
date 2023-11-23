909155b58f72bd865c02431b3946b1e0e433f434
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-23 11:28:23 +0530

    setTimeout function for callback

--- callBackFunction.js
+++ callBackFunction.js
@@ -1,12 +1,18 @@
 //Call Back Function :
 // a function passing a function as a parameter
+//And this called callback function means that when we required that function we can callback it to execute
 //ex 
+
+setTimeout(function()
+{
+   console.log("after 5 sec print it");
+},5000);
 
 function x(z)
 {
    console.log("Hello");
    z();
-   
+
 }
 
 x(function y(){
