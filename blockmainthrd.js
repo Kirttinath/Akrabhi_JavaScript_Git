6edfb624f0af71b2dde005863a7a26b07278fd2a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-24 19:45:16 +0530

    Blocking the main Thread that currupts the output of settimeout

--- blockmainthrd.js
+++ blockmainthrd.js
@@ -6,3 +6,13 @@ console.log("Started");
  }, 5000);
 
 console.log("Ended");
+
+// Block Main Thread
+
+let startdt = new Date().getTime();
+let enddt = startdt;
+while(enddt < startdt+10000)
+{
+    enddt = new Date().getTime();
+}
+console.log("This is before settimeout");
