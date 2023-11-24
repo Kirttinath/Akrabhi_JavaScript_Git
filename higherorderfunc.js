122e9c41e3e3982587c88b5a77fdc391144df6b3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-24 20:32:40 +0530

    creating array and take array elements as radius and find area

--- higherorderfunc.js
+++ higherorderfunc.js
@@ -3,13 +3,33 @@
 //or Return a function or Both
 //for ex  : ForEach Method alwaays takes another function inside it
 //So forEach is a higher order function
-function f(val)
-{
-     
-}
-f( function fx(){
+function f(val) {
+    val();
+    return function g() { console.log("Ojha"); };
 
-})
+}
+
+var gres = f(function fx() {
+    console.log("Kirtti");
+
+});
+gres();
 //or ex:
-var arr = [1,2,3,4,5];
-arr.forEach(function(){})
+var arr = [1, 2, 3, 4, 5];
+arr.forEach(function () { })
+
+
+const radius = [3, 4, 5, 2];
+function findArea(radius) {
+  const res =[];
+  for(let i=0;i < radius.length; i++)
+  {
+    res.push(Math.PI*radius[i]*radius[i]);
+  }
+  return res;
+}
+
+console.log(findArea(radius));
+//and similarly create for find diameter and circumference
+//That would be not a good programming skill to repeat the code 
+//so the optimized and good programming skill would be
