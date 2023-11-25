9d06e33be19a199aed6caac91216c1ffe464e2bb
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-25 12:15:36 +0530

    optimized way code using higher order functions

--- higherorderfunc.js
+++ higherorderfunc.js
@@ -4,13 +4,13 @@
 //for ex  : ForEach Method alwaays takes another function inside it
 //So forEach is a higher order function
 function f(val) {
-    val();
-    return function g() { console.log("Ojha"); };
+  val();
+  return function g() { console.log("Ojha"); };
 
 }
 
 var gres = f(function fx() {
-    console.log("Kirtti");
+  console.log("Kirtti");
 
 });
 gres();
@@ -19,17 +19,43 @@ var arr = [1, 2, 3, 4, 5];
 arr.forEach(function () { })
 
 
-const radius = [3, 4, 5, 2];
-function findArea(radius) {
-  const res =[];
-  for(let i=0;i < radius.length; i++)
-  {
-    res.push(Math.PI*radius[i]*radius[i]);
-  }
-  return res;
-}
 
-console.log(findArea(radius));
+// function findArea(radius) {
+//   const res =[];
+//   for(let i=0;i < radius.length; i++)
+//   {
+//     res.push(Math.PI*radius[i]*radius[i]);
+//   }
+//   return res;
+// }
+
+// console.log(findArea(radius));
 //and similarly create for find diameter and circumference
 //That would be not a good programming skill to repeat the code 
 //so the optimized and good programming skill would be
+const radius = [3, 4, 5, 2];
+
+const area = function (radius) {
+  return Math.PI * radius * radius;
+}
+
+const diameter = function (radius) {
+  return 2 * radius;
+}
+
+const circumference = function (radius) {
+ return 2*Math.PI*radius;   
+}
+
+function calculate(radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+}
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
+
