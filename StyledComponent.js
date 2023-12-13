351d993947f560f9bf85c7179cbe0e9cc6ececae
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-13 22:55:55 +0530

    React Styled Component Documentation

---
+++ StyledComponent.js
@@ -0,0 +1,36 @@
+/* 
+     ✅INTRODUCTION
+    ..............
+
+📌What is Styled Component : 
+--------------------------
+
+Styled components ia a css-in js styling solution for React and React native
+It is an library in the react ecosystem
+ 
+📌Features / Benefits : 
+-----------------------
+1.Automatic Critical CSS
+2.No class name Bugs
+3.Easier deletion of CSS
+4.Dynamic Styling
+5.Painless Maintenance
+6.Automatic vendor Prefixing
+
+📌Prerequisites :
+--------------------
+*Know Fundamentals Of Reacts
+
+
+     ✅ INSTALLATION & SET UP
+      ........................
+
+1. npx create-react-app projectname
+2.VS code styled component extension install
+3.npm install styled-components
+
+
+     ✅ BASIC STYLED COMPONENT
+    ..........................
+   Go to 👉 📌CODEWORLD REPO
+*/ 
