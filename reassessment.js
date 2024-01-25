c7a9953c5f01fb886bc930631b7201b483ed5d6f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-26 00:35:38 +0530

    added

--- reassessment.js
+++ reassessment.js
@@ -1,50 +1,8 @@
-// const languageObj = {name: "JavaScript", age:26};
-// _languageObj = languageObj;
+console.log(b);
 
-// _languageObj.age = "I don't know";
-
-// console. log(languageObj);
-// console. log(_languageObj);
-
-// const numbers = [1, 2, 3, 4, 5];
-// const _numbers = [...numbers];
-// _numbers[0] = 10;
-// console.log(numbers); //[1,2,3,4,5]
-// console.log(_numbers);//[10,2,3,4,5]
-
-// function cb(cm)
-// {
-//     cm();
-// }
-
-//  var cm = function cc()
-// {
-//     console.log("Kirtti");
-// }
-// const numbers = [1, 2, 3, 4, 5];
-
-// console.log(numbers.map(x => x+5));
-// console.log(numbers);
-// numbers.forEach(function()
-// {
-//     y = y+5;
-// })
-// var output = (function(x) { delete x; 
-//     return x; })(0);
-//      console.log(output);
-// var x;
-// console.log(x);
-// console.log(y);
-// let  numbers = [1, 2, 3, 4, 5];
-// console.log(numbers.slice(1,3));
-// console.log(numbers);
-// numbers.splice(1,3);
-// var set = new Set();
-// set.add(1);
-// set.add(2);
-// set.add(3);
-// set.add(2);
-
-// console.log(set)
-console.log(x);
-var x=10;
+let a = 10;
+var b = 20;
+a = 40;
+console.log(a);
+const c = 30;
+console.log(`${a} ${b} ${c}`);
--- restsyntax.js
+++ restsyntax.js
@@ -1,7 +1,7 @@
 // Rest syntax :
 // use it to put any number of arguments passed to a function into an array
 function myFunc(...args) {
-  return (args[0] + args[1]);
+  return args[0] + args[1];
 }
- var res = myFunc(5, 5, 3, 4);
- console.log(res)// 10
+var res = myFunc(5, 5, 3, 4);
+console.log(res); // 10
