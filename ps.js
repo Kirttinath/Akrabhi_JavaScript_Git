c60bc4d2c226a90698c61928b7151817142c472f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-26 13:02:17 +0530

    some practice

--- ps.js
+++ ps.js
@@ -5,11 +5,11 @@ var b = 20;
 a = 40;
 console.log(a);
 const c = 30;
-console.log(`${a} ${b} ${c}`);
 
 //* let and var are function scoped, const is block scoped
-
 {
-  var k = 20;
+  let k = 20;
 }
+console.log(`${a} ${b} ${c}`);
 console.log(k);
+console.log("Kirttinath");
