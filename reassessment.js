2a2fb26c9503af806c5124e55c8e3799046f73b1
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-26 14:46:56 +0530

    rest parameter used

--- reassessment.js
+++ reassessment.js
@@ -5,7 +5,7 @@ var b = 20;
 a = 40;
 console.log(a);
 const c = 30;
-console.log(`${a} ${b} ${c}`);
+console.log(a, b, c);
 
 //* let and var are function scoped, const is block scoped
 
@@ -13,3 +13,13 @@ console.log(`${a} ${b} ${c}`);
   var k = 20;
 }
 console.log(k);
+
+const arr = [1, 2, 3, 4];
+const arr1 = [...arr, 5, 6, 7];
+console.log(arr1);
+function s(x, y, ...a) {
+  console.log(x, y);
+  return a.reduce((sum, num) => sum + num, 0);
+}
+let result = s(1, 2, 3, 4, 5);
+console.log(result);
