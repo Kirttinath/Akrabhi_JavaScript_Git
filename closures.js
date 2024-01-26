acb24ef8d7237be16a387eb32274a5234e0eb711
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-26 18:55:42 +0530

    Refactor code for closures.js

--- closures.js
+++ closures.js
@@ -1,48 +1,41 @@
-//closures :
-//Closure is a function object that has access to its own scope, the outer function’s variables. Three characteristics of closures are:
+//* closures :
+//* Closure is a function object that has access to its own scope, the outer function’s variables. Three characteristics of closures are:
 
-const a=10;
-const f = () => 
-{
-  const a=23;
-  const f2 = () =>
-  {
-    console.log(a);//23
-  }
+const a = 10;
+const f = () => {
+  const a = 23;
+  const f2 = () => {
+    console.log(a); //23
+  };
   f2();
-}
+};
 f();
-console.log(a);//10
+console.log(a); //10
 
-//Closures are used when you want to extend behavior such as passing variables, methods, or arrays from an outer function to an inner function.
-//JavaScript is not pure object-oriented language but you can achieve object-oriented behavior through closures
+//? Closures are used when you want to extend behavior such as passing variables, methods, or arrays from an outer function to an inner function.
+//? JavaScript is not pure object-oriented language but you can achieve object-oriented behavior through closures
 
+//todo ==> closure access
 
-//closure access
-  function counter()
-  {
-    var count =0;
-    this.incrementcounter = function()
-    {
-      count++;
-      console.log(count);
-    }
-    this.decrementcounter = function()
-    {
-      count--;
-      console.log(count);
-    }
-  }
- var cn = new  counter();
-for(let i=1; i<=50;i++)
-{
+function counter() {
+  var count = 0;
+  this.incrementcounter = function () {
+    count++;
+    console.log(count);
+  };
+  this.decrementcounter = function () {
+    count--;
+    console.log(count);
+  };
+}
+var cn = new counter();
+for (let i = 1; i <= 50; i++) {
   cn.incrementcounter();
   cn.decrementcounter();
-}//50 times 1 and 0 values are printed
+}
 
-//Disadvantages of closures :
-//1) memory leakage: if we have a lot of nested functions, it can cause the browser to run out of memory.
-//2) scope chain pollution: when you declare variables in a function that is inside another function, these variables become part of the outer function'
+//* 50 times 1 and 0 values are printed
 
-
-
+//! Disadvantages of closures :
+//! 1) memory leakage: if we have a lot of nested functions, it can cause the browser to run out of memory.
+//! 2) scope chain pollution: when you declare variables in a function that is inside another function, these variables become part of the outer function'
