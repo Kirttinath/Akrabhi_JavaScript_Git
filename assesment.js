9f1700d97b69b4e64aa62709ec7d00f57ef3c270
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-26 16:01:20 +0530

    practice js

--- assesment.js
+++ assesment.js
@@ -1,51 +1,56 @@
 //promise function
 
-  
-  let myPromise = new Promise(function(myResolve, myReject) {
-    let x = 1;
-  
+let myPromise = new Promise(function (myResolve, myReject) {
+  let x = 1;
+
   // The producing code (this may take some time)
-  
-    if (x == 0) {
-      myResolve("OK");
-    } else {
-      myReject("Error");
-    }
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.catch(
+  function (value) {
+    console.log(value);
+  },
+  function (error) {
+    console.log(error);
+  }
+);
+
+//asynchronous
+async function myFunction() {
+  return "Hello";
+}
+myFunction().then(
+  function (value) {
+    console.log(value);
+  },
+  function (error) {
+    console.log(error);
+  }
+);
+
+//await
+async function myDisplay() {
+  let myPromise = new Promise(function (resolve, reject) {
+    resolve("Kirtti");
   });
-  
-  myPromise.catch(
-    function(value) {console.log(value);},
-    function(error) {console.log(error);}
-  );
+  console.log(await myPromise);
+}
 
-  //asynchronous 
-  async function myFunction() {
-    return "Hello";
-  }
-  myFunction().then(
-    function(value) {console.log(value);},
-    function(error) {console.log(error);}
-  );
-  
+myDisplay();
 
-  //await
-  async function myDisplay() {
-    let myPromise = new Promise(function(resolve, reject) {
-      resolve("Kirtti");
-    });
-    console.log( await myPromise);
-  }
-  
-  myDisplay();
+//
+function myFirst() {
+  console.log("Kirttinath");
+}
 
-  //
-  function myFirst() {
-    console.log("Kirttinath");
-  }
-  
-  function mySecond() {
-    console.log("Goodbye");
-  }
-  mySecond();//this will execute first
-  myFirst();//second this is execute
-  
+function mySecond() {
+  console.log("Goodbye");
+}
+mySecond(); //this will execute first
+myFirst(); //second this is execute
--- closure.js
+++ closure.js
@@ -1,44 +1,44 @@
 function f1() {
-    var a = 100;
-    /*return function f2(){
+  var a = 100;
+  /*return function f2(){
         console.log(a);
 
     }*/
-    function f2() {
-        console.log(a);
-    }
+  function f2() {
+    console.log(a);
+  }
 
-    return f2;
-
+  return f2;
 }
-var c = f1();
-console.log(c);
-c();
+f1()();
 
-//Closures :
-//Closure is an important JavaScript pattern to give private access to a variable
+//? Closures :
+//? Closure is an important JavaScript pattern to give private access to a variable
+
 function closures(val) {
-    return function (name) {
-        console.log(val + " " + name);
-    }
+  return function (name) {
+    console.log(val + " " + name);
+  };
 }
 const greet = closures("Hello");
 greet("Kirtti");
 
-//In a more real-world scenario, you could envision an initial function apiConnect(apiKey) that returns some methods that would use the API key. In this case, the apiKey would just need to be provided once and never again.
+//* In a more real-world scenario, you could envision an initial function apiConnect(apiKey) that returns some methods that would use the API key. In this case, the apiKey would just need to be provided once and never again.
 
 function apiConnect(apiKey) {
-    function get(route) {
-        return fetch(`${route}?key=${apiKey}`);
-    }
+  function get(route) {
+    return fetch(`${route}?key=${apiKey}`);
+  }
 
+  function post(route, params) {
+    return fetch(route, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: { Authorization: `Bearer ${apiKey}` },
+    });
+  }
 
-
-    function post(route, params) { 
-        return fetch(route, { method: 'POST', body: JSON.stringify(params), headers: { 'Authorization': `Bearer ${apiKey}` } }) }
-
-
-    return { get, post }
+  return { get, post };
 }
 
-const api = apiConnect('my-secret-key');
+const api = apiConnect("my-secret-key");
