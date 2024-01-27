931cf83d6734390c8d535758224bcba86f67a9fb
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-27 09:58:39 +0530

    Add async function to use await

--- asyncawait.js
+++ asyncawait.js
@@ -6,6 +6,7 @@
 const greeter = new Promise((res, rej) => {
   setTimeout(() => res("Hello world!"), 2000);
 });
+
 async function myFunc() {
   const greeting = await greeter;
   console.log(greeting);
