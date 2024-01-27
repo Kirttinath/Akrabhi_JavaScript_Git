c1dd994b61f54326e69b7505e95c116441f3c1b3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-27 17:56:39 +0530

    Added a callback function example to the reassessment.js file.

--- reassessment.js
+++ reassessment.js
@@ -1,5 +1,15 @@
-var a = 10;
-function test() {
-  var b = 17;
+//* -------- CallBack function --------
+//? Callback function is a function passed as an argument to another function and it executes after the completion of the main function.
+
+function greet(name, callback) {
+  console.log(`Hello ${name}`);
+  setTimeout(() => {
+    callback();
+  }, 5000);
 }
-test();
+
+function goodbye() {
+  console.log("Goodbye ..");
+}
+
+greet("Kirtti", goodbye);
