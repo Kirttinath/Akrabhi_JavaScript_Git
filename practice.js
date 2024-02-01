dec720edc7d8e58803040e8db805aa378bcfead9
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-01 22:39:11 +0530

    prototype in javascript

--- practice.js
+++ practice.js
@@ -1,10 +1,6 @@
-let x  = 9;
-{
-    let x = 5;
-    var z = 10;
-    console.log("The value of the x = "+x);  // value will be 5 insie the block
-}
-console.log("The value of the x = "+x);    //we can't access {x}outside the block 
-                                           //here the value of x is 9 outside the block
-                                           
-console.log("The value of the z = "+z);       //we can access the variable z outside the block
+const list = [];
+const car = {};
+console.log(car._proto_ === new Object()._proto_);
+console.log(list._proto_ === new Array()._proto_);
+console.log(car._proto_ === Object.Prototype);
+console.log(list._proto_ === Array.Prototype);
