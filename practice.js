75738d72d66bd0a490f19da287d69d55ee2f7407
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-02 00:04:20 +0530

    setInterval added

--- practice.js
+++ practice.js
@@ -1,13 +1,5 @@
-const result = new Promise((result, reject) => {
-  let num = 2;
-  if (num % 2 == 0) {
-    result(num + " is an even number");
-  } else reject(num + " is not an even number");
-});
-result
-  .then(function (data) {
-    console.log("success : " + data);
-  })
-  .catch(function (err) {
-    console.log("error : " + err);
-  });
+const inter = function () {
+  console.log("Hello");
+};
+
+setTimeout(inter, 1000);
