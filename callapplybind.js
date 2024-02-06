c88da7bc9e2859639178b79194d3502ba396fe5a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-06 16:24:17 +0530

    callapplybind methods examples

--- callapplybind.js
+++ callapplybind.js
@@ -7,15 +7,15 @@ function newf(val1, val2, val3) {
 var obj = {
   age: 24,
 };
-newf.call(obj, 1, 2, 3);
+// newf.call(obj, 1, 2, 3);
 
-//* apply :
-newf.apply(obj, [1, 2, 3]); //gives same values
+// //* apply :
+// newf.apply(obj, [1, 2, 3]); //gives same values
 
 //* It says that when passing object as argument we should pass 2 arguments so we pass object and function parameters as an array
 
 //* Bind :
 var bindObj = newf.bind(obj, 1, 2, 3);
 //* newf.bind(obj,1,2,3); gives the function which we should store for display it
-
-console.log(bindObj); // it invoked and give the result
+bindObj();
+// console.log(bindObj); // it invoked and give the result
